b69bd30abe10c0d91e2f08d7f999a014ba013ecc
rohildshah <shah5963@gmail.com>
2020-07-25 12:35:13 -0700

    User input signin attempt

--- api/webhook.js
+++ api/webhook.js
@@ -1,11 +1,38 @@
 'use strict';
 
 // Import dependencies
+/* eslint-disable */
+import * as firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+
+const config = {
+  apiKey: "AIzaSyCKbckfbi7xKdG7Q368tdHflPYxpMiDcHM",
+  authDomain: "hackathon-3cd54.firebaseapp.com",
+  databaseURL: "https://hackathon-3cd54.firebaseio.com",
+  projectId: "hackathon-3cd54",
+  storageBucket: "hackathon-3cd54.appspot.com",
+  messagingSenderId: "279651007594",
+  appId: "1:279651007594:web:43590bce5c69595b1d5e13",
+  measurementId: "G-KN56T860B9"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express().use(bodyParser.json());
 const request = require('request');
-const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+// const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ACCESS_TOKEN = "EAAmhWoMcs2EBAOqJ3NYaE6FkAoccKPxoyVIsLsRbhOyi7hGqvBMB0MlQV0ZBh8RDYOhiDMVrzHUR8Pku0pTkxnQmF71ptPHfhpdcrvobi93aKXtMm41DUic4vLE5yJdC49zjU040sKLCujTgmQgUnrPN2KZAZAu5Udfyl09KgZDZD";
+const user = "";
+const passkey = "";
+const queries = [];
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
@@ -75,16 +102,54 @@ function handleMessage(sender_psid, received_message) {
   let response;
   let text = received_message.text;
   let attachment = received_message.attachments;
+  console.log(text)
 
   // Check if message has text
   if (text) {
-    response = {
-      "text": `You sent the message: "${text}". Direct messaging is not supported at this time.`
+    user = "";
+    passkey = "";
+    queries = [];
+    queries = text.split(" ");
+    console.log(queries)
+    switch (queries[0]) {
+      case 'user:':
+        user = queries[1]
+        response = {
+          "text": `Your new username is: "${queries[1]}". Please type a username with the format 'pk: [my-passkey]'.`
+        }
+        break;
+      case 'pk:':
+        passkey = queries[1]
+        response = {
+          "attachment": {
+            "type": "template",
+            "payload": {
+              "template_type": "button",
+              "text": `Log in or Sign up with "${user}" and "${passkey}"?`,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Log In",
+                  "payload": "log"
+                },
+                {
+                  "type": "postback",
+                  "title": "Sign Up",
+                  "payload": "sign"
+                }
+              ]
+            }
+          }
+        }
+        break;
+      default:
+        response = {
+          "text": "Invalid"
+        }
     }
-
   } else if (attachment) {
     response = {
-      "text": "Please do not send attachments."
+      "text": "Attachments are not supported at this time. Please choose an option from the hamburger menu."
     }
   }
 
@@ -111,13 +176,8 @@ function handlePostback(sender_psid, received_postback) {
               "buttons": [
                 {
                   "type": "postback",
-                  "title": "Sign Up",
-                  "payload": "sign",
-                },
-                {
-                  "type": "postback",
-                  "title": "Log In",
-                  "payload": "log"
+                  "title": "Sign Up or Log In",
+                  "payload": "auth",
                 }
               ],
             }]
@@ -125,67 +185,58 @@ function handlePostback(sender_psid, received_postback) {
         }
       }
       break;
+    case 'auth':
+      response = {
+        "text": "Please type a username with the format 'user: [my-username]'."
+      }
+      break;
+    case 'log':
+      fbAuth('log');
+      break;
     case 'sign':
+      fbAuth('sign');
+      break;
+    case 'settings':
       response = {
         "attachment": {
           "type": "template",
           "payload": {
             "template_type": "button",
-            "text": "Create a new acount with",
+            "text": "Edit your settings here!",
             "buttons": [
               {
                 "type": "web_url",
                 "url": "https://www.facebook.com",
                 "title": "Facebook"
-              },
-              {
-                "type": "web_url",
-                "url": "https://www.google.com",
-                "title": "Google"
-              },
-              {
-                "type": "web_url",
-                "url": "https://mail.google.com",
-                "title": "Email"
-              },
+              }
             ]
           }
         }
       }
       break;
-    case 'log':
-      response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "button",
-            "text": "Log in to an existing account with",
-            "buttons": [
-              {
-                "type": "web_url",
-                "url": "https://www.facebook.com",
-                "title": "Facebook"
-              },
-              {
-                "type": "web_url",
-                "url": "https://www.google.com",
-                "title": "Google"
-              },
-              {
-                "type": "web_url",
-                "url": "https://mail.google.com",
-                "title": "Email"
-              },
-            ]
-          }
-        }
-      }
+    case 'report':
       break;
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
 
+async function fbAuth(loginType) {
+  if (loginType == 'log') {
+    console.log(account.password)
+    console.log(account.email)
+    await auth.signInWithEmailAndPassword(account.email, account.password)
+    const { email, uid } = auth.currentUser
+  } else {
+    await auth.createUserWithEmailAndPassword(account.email, account.password)
+    const { email, uid } = auth.currentUser
+    await db.collection("users").add({
+      name: account.name,
+      email: email,
+      uid: uid
+    })
+  }
+}
 
 function callSendAPI(sender_psid, response) {
   // Construct message body
@@ -214,4 +265,3 @@ module.exports = {
   path: '/api',
   handler: app
 }
-
